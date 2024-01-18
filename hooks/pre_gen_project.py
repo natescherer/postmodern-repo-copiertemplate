2140de2e0b20fd4e3d94f28253c0a60a7728f361
Nate Scherer <376408+natescherer@users.noreply.github.com>
2024-01-17 22:51:57 -0500

    Block unnecessary pieces for CI execution

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -19,153 +19,156 @@ class color:
 
 print(color.GREEN + "*** PRE-GEN PROJECT HOOK START ***" + color.END)
 
-print(color.CYAN + "Verifying GitHub CLI is installed..." + color.END)
-if not shutil.which("gh"):
-    raise FileNotFoundError("GitHub CLI is required and is not installed!")
+if os.environ.get("CI") == "true":
+    print(color.YELLOW + "Skipping GitHub CLI steps as script appears to be running in GitHub Actions" + color.END)
+else:
+    print(color.CYAN + "Verifying GitHub CLI is installed..." + color.END)
+    if not shutil.which("gh"):
+        raise FileNotFoundError("GitHub CLI is required and is not installed!")
 
-print(color.CYAN + "Verifying you are logged in to GitHub CLI..." + color.END)
-try:
-    subprocess.run(["gh", "auth", "status"], capture_output=True, check=True)
-except subprocess.CalledProcessError as exc:
-    raise Exception("You are not logged into gh cli, run gh auth login") from exc
+    print(color.CYAN + "Verifying you are logged in to GitHub CLI..." + color.END)
+    try:
+        subprocess.run(["gh", "auth", "status"], capture_output=True, check=True)
+    except subprocess.CalledProcessError as exc:
+        raise Exception("You are not logged into gh cli, run gh auth login") from exc
 
-if "{{cookiecutter.new_project}}" == "Yes":
-    print(color.CYAN + "Creating & Cloning GitHub repo..." + color.END)
-    gh_repo_create = subprocess.run(
+    if "{{cookiecutter.new_project}}" == "Yes":
+        print(color.CYAN + "Creating & Cloning GitHub repo..." + color.END)
+        gh_repo_create = subprocess.run(
+            [
+                "gh",
+                "repo",
+                "create",
+                "{{cookiecutter.repo_name}}",
+                "--public",
+                "--clone",
+                "--add-readme"
+            ],
+            capture_output=True,
+            check=True,
+            cwd="..",
+        )
+
+    print(color.CYAN + "Setting GitHub repo settings..." + color.END)
+    repo_settings = {
+        "description": "{{cookiecutter.short_description}}",
+        "homepage": "{{cookiecutter.project_website}}",
+        "allow_auto_merge": True,
+        "delete_branch_on_merge": True,
+    }
+    gh_api_repos = subprocess.run(
         [
             "gh",
-            "repo",
-            "create",
-            "{{cookiecutter.repo_name}}",
-            "--public",
-            "--clone",
-            "--add-readme",
+            "api",
+            "/repos/{owner}/{repo}",
+            "--method",
+            "PATCH",
+            "-H",
+            "Accept: application/vnd.github+json",
+            "-H",
+            "X-GitHub-Api-Version: 2022-11-28",
+            "--input",
+            "-"
         ],
         capture_output=True,
         check=True,
-        cwd="..",
+        input=json.dumps(repo_settings).encode()
     )
 
-print(color.CYAN + "Setting GitHub repo settings..." + color.END)
-repo_settings = {
-    "description": "{{cookiecutter.short_description}}",
-    "homepage": "{{cookiecutter.project_website}}",
-    "allow_auto_merge": True,
-    "delete_branch_on_merge": True,
-}
-gh_api_repos = subprocess.run(
-    [
-        "gh",
-        "api",
-        "/repos/{owner}/{repo}",
-        "--method",
-        "PATCH",
-        "-H",
-        "Accept: application/vnd.github+json",
-        "-H",
-        "X-GitHub-Api-Version: 2022-11-28",
-        "--input",
-        "-",
-    ],
-    capture_output=True,
-    check=True,
-    input=json.dumps(repo_settings).encode(),
-)
+    print(color.CYAN + "Adding GitHub Labels..." + color.END)
+    gh_label_create_awaiting_pr = subprocess.run(
+        [
+            "gh",
+            "label",
+            "create",
+            "awaiting pr",
+            "-d",
+            "Awaiting completion of a PR from a contributor",
+            "-c",
+            "668F04",
+            "-f"
+        ],
+        capture_output=True,
+        check=True
+    )
+    gh_label_create_awaiting_pr = subprocess.run(
+        [
+            "gh",
+            "label",
+            "create",
+            "blocked",
+            "-d",
+            "Blocked by an external dependency",
+            "-c",
+            "B60205",
+            "-f"
+        ],
+        capture_output=True,
+        check=True
+    )
 
-print(color.CYAN + "Adding GitHub Labels..." + color.END)
-gh_label_create_awaiting_pr = subprocess.run(
-    [
-        "gh",
-        "label",
-        "create",
-        "awaiting pr",
-        "-d",
-        "Awaiting completion of a PR from a contributor",
-        "-c",
-        "668F04",
-        "-f",
-    ],
-    capture_output=True,
-    check=True,
-)
-gh_label_create_awaiting_pr = subprocess.run(
-    [
-        "gh",
-        "label",
-        "create",
-        "blocked",
-        "-d",
-        "Blocked by an external dependency",
-        "-c",
-        "B60205",
-        "-f",
-    ],
-    capture_output=True,
-    check=True,
-)
+    print(color.CYAN + "Setting GitHub branch protection ruleset..." + color.END)
+    repo_ruleset = {
+        "name": "default-branch-protection",
+        "enforcement": "active",
+        "target": "branch",
+        "bypass_actors": [],
+        "conditions": {"ref_name": {"exclude": [], "include": ["~DEFAULT_BRANCH"]}},
+        "rules": [
+            {"type": "deletion"},
+            {"type": "non_fast_forward"},
+            {
+                "type": "pull_request",
+                "dismiss_stale_reviews_on_push": False,
+                "require_code_owner_review": False,
+                "require_last_push_approval": False,
+                "required_approving_review_count": 0,
+                "required_review_thread_resolution": False
+            }
+        ]
+    }
+    gh_api_repos_rulesets = subprocess.run(
+        [
+            "gh",
+            "api",
+            "/repos/{owner}/{repo}/rulesets",
+            "--method",
+            "POST",
+            "-H",
+            "Accept: application/vnd.github+json",
+            "-H",
+            "X-GitHub-Api-Version: 2022-11-28",
+            "--input",
+            "-"
+        ],
+        capture_output=True,
+        check=True,
+        input=json.dumps(repo_ruleset).encode()
+    )
 
-print(color.CYAN + "Setting GitHub branch protection ruleset..." + color.END)
-repo_ruleset = {
-    "name": "default-branch-protection",
-    "enforcement": "active",
-    "target": "branch",
-    "bypass_actors": [],
-    "conditions": {"ref_name": {"exclude": [], "include": ["~DEFAULT_BRANCH"]}},
-    "rules": [
-        {"type": "deletion"},
-        {"type": "non_fast_forward"},
-        {
-            "type": "pull_request",
-            "dismiss_stale_reviews_on_push": False,
-            "require_code_owner_review": False,
-            "require_last_push_approval": False,
-            "required_approving_review_count": 0,
-            "required_review_thread_resolution": False,
-        },
-    ],
-}
-gh_api_repos_rulesets = subprocess.run(
-    [
-        "gh",
-        "api",
-        "/repos/{owner}/{repo}/rulesets",
-        "--method",
-        "POST",
-        "-H",
-        "Accept: application/vnd.github+json",
-        "-H",
-        "X-GitHub-Api-Version: 2022-11-28",
-        "--input",
-        "-",
-    ],
-    capture_output=True,
-    check=True,
-    input=json.dumps(repo_ruleset).encode(),
-)
-
-print(color.CYAN + "Setting GitHub Actions settings..." + color.END)
-actions_rules = {
-    "can_approve_pull_request_reviews": True,
-    "default_workflow_permissions": "write"
-}
-gh_api_actions_rules = subprocess.run(
-    [
-        "gh",
-        "api",
-        "/repos/{owner}/{repo}/actions/permissions/workflow",
-        "--method",
-        "PUT",
-        "-H",
-        "Accept: application/vnd.github+json",
-        "-H",
-        "X-GitHub-Api-Version: 2022-11-28",
-        "--input",
-        "-",
-    ],
-    capture_output=True,
-    check=True,
-    input=json.dumps(actions_rules).encode(),
-)
+    print(color.CYAN + "Setting GitHub Actions settings..." + color.END)
+    actions_rules = {
+        "can_approve_pull_request_reviews": True,
+        "default_workflow_permissions": "write"
+    }
+    gh_api_actions_rules = subprocess.run(
+        [
+            "gh",
+            "api",
+            "/repos/{owner}/{repo}/actions/permissions/workflow",
+            "--method",
+            "PUT",
+            "-H",
+            "Accept: application/vnd.github+json",
+            "-H",
+            "X-GitHub-Api-Version: 2022-11-28",
+            "--input",
+            "-"
+        ],
+        capture_output=True,
+        check=True,
+        input=json.dumps(actions_rules).encode()
+    )
 
 # Choose correct files based on project type
 PROJECT = "{{cookiecutter.project_type}}"
