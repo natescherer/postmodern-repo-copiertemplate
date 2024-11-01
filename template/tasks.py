ee2ddf7212ab62b420bf7579086d28b15d183352
Nate Scherer <376408+natescherer@users.noreply.github.com>
2024-11-01 20:50:46 +0000

    fix: fix payload for azdo pipeline creation

--- template/tasks.py
+++ template/tasks.py
@@ -245,9 +245,9 @@ def create_pipelines_azdo(c, answers_json):
     for entry in os.scandir(".azurepipelines"):
         if entry.name.endswith(".yml"):
             pipeline_data = {
-                "name": answers["repo_name"],
+                "name": f"{answers['repo_name']} {Path(entry.name).with_suffix('')}",
                 "repository": {
-                    "name": f"{answers['repo_name']} {Path(entry.name).with_suffix('')}",
+                    "name": answers["repo_name"],
                     "type": "TfsGit",
                 },
                 "process": {"yamlFilename": f".azurepipelines/{entry.name}", "type": 2},
