29dbfa940396d167cb8570690c8703050131b84a
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-01-08 19:55:14 -0500

    feat: add default for repo_name question

--- template/{% if is_template %}extensions{% endif %}/path_extension.py
+++ template/{% if is_template %}extensions{% endif %}/path_extension.py
@@ -1,4 +1,4 @@
-"""Provide extension for testing path validity."""
+"""Provide extensions relating to paths."""
 
 import os
 
@@ -6,18 +6,22 @@ from jinja2.ext import Extension
 
 
 def path_exists(path):
-    """Check if a path exists, and returns True if it does and nothing if it doesn't."""
+    """Check if a path exists, and return True if it does and nothing if it doesn't."""
     if os.path.exists(path):
         return True
 
 
 def path_missing(path):
-    """Check if a path exists, and returns True if it does NOT and nothing if it does."""
+    """Check if a path exists, and return True if it does NOT and nothing if it does."""
     if not os.path.exists(path):
         return True
 
+def cwd_name():
+    """Return the name of the current working directory."""
+    return os.path.basename(os.getcwd())
 
-class PathTestsExtension(Extension):
+
+class PathExtension(Extension):
     """Export for use by Jinja."""
 
     def __init__(self, environment):
@@ -25,3 +29,4 @@ class PathTestsExtension(Extension):
         super().__init__(environment)
         environment.filters["path_exists"] = path_exists
         environment.filters["path_missing"] = path_missing
+        environment.globals["cwd_name"] = cwd_name
