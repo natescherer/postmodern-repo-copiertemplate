48f412e3c07b36e35adbbd75d20024ecac784cfe
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-08-13 19:07:10 +0000

    fix: specify git local clone

--- template/tasks.py
+++ template/tasks.py
@@ -33,12 +33,15 @@ def copy_template_files(c, src_path, vcs_ref):
             print(f"src_path: {src_path}")
             time.sleep(5)
             c.run(
-                f"git -c advice.detachedHead=false clone -q "
-                f"--branch {vcs_ref} '{src_path}' {tmpdir}"
+                f"git -c advice.detachedHead=false clone --quiet --local "
+                f"--branch {vcs_ref} {src_path} {tmpdir}"
             )
         else:
             print("Checkpoint 2")
-            c.run(f"git -c advice.detachedHead=false clone -q '{src_path}' {tmpdir}")
+            c.run(
+                f"git -c advice.detachedHead=false clone --quiet --local "
+                f"{src_path} {tmpdir}"
+            )
         print("Checkpoint 3")
         shutil.copytree(f"{tmpdir}/template", "template", dirs_exist_ok=True)
     print("[bold green]*** 'copy-template-files' task end ***[/bold green]")
